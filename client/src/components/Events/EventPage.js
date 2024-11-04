80de871529746c8e70a5a0c57028ad4fd129252f
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-03 22:14:30 -0600

    Updated the card component

--- client/src/components/Events/EventPage.js
+++ client/src/components/Events/EventPage.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './EventPage.css'; // Make sure to include your CSS file
+import "./EventPage.css"
 
 const EventPage = () => {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState('');
+  const a = "3443";
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -49,23 +50,28 @@ const EventPage = () => {
     <div className="container mt-5">
       <h1 className="text-center">Event Pages</h1>
       <div className="row">
-        {events.map(event => (
-          <div className="col-md-4" key={event.id}>
+        {events.map((event) => (
+          <div className="col-md-4 " key={event.id}>
             <div className="card mt-4 event-card">
+              <div className="card-img-overlay d-flex justify-content-end align-items-start">
+                <span className="badge badge-primary">{event.status}</span>
+              </div>
               <img
                 src={defaultImage}
                 className="card-img-top"
                 alt={event.name}
+                style={{borderRadius: "8px"}}
               />
-              <div className="card-body">
-                <h5 className="card-title">{event.name}</h5>
-                <p className="card-text">Date: {event.date}</p>
+              <div className="card-body custom-card-body">
+                <h5 className="card-title " style={{}}>{event.title}</h5>
+                <p className="card-text">Date: {event.startTime}</p>
                 <p className="card-text">Location: {event.location}</p>
                 <p className="card-text">{event.description}</p>
                 <button className="btn btn-primary">Reserve Spot</button>
               </div>
             </div>
           </div>
+
         ))}
       </div>
     </div>
