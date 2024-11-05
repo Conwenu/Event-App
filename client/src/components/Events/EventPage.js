3eed52bee0f78d2630481c2e8b6fc3e54e53fb9d
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-05 16:03:36 -0600

    reformatted it

--- client/src/components/Events/EventPage.js
+++ client/src/components/Events/EventPage.js
@@ -1,18 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import "./EventPage.css"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./EventPage.css";
+import { Badge } from "react-bootstrap";
+import EventHelperFunctions from "./EventHelperFunctions.js";
+import EventCard from "./EventCard.js";
+const API_URL = process.env.REACT_APP_API_URL;
+
+function formatDate(isoTime) {
+  const date = new Date(isoTime);
+
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${month}/${day}/${year}`;
+}
+
+function getMonth(isoTime) {
+  const date = new Date(isoTime);
+  var monthName = new Intl.DateTimeFormat("en-US", { month: "long" }).format;
+  var longName = monthName(date); 
+  return `${longName}`
+}
 
 const EventPage = () => {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [error, setError] = useState('');
-  const a = "3443";
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get('http://localhost:3050/api/events');
+        const response = await axios.get(`${API_URL}/events`);
         setEvents(response.data.events);
       } catch (err) {
         setIsError(true);
@@ -25,57 +45,45 @@ const EventPage = () => {
     fetchEvents();
   }, []);
 
-  const defaultImage = 'https://wallpapercat.com/w/full/6/2/1/116007-3840x2160-desktop-4k-bleach-wallpaper-photo.jpg';
+  const defaultImage =
+    "https://wallpapercat.com/w/full/6/2/1/116007-3840x2160-desktop-4k-bleach-wallpaper-photo.jpg";
 
-  if (isLoading) return (
-    <div className="loading-container">
-      <div className="spinner"></div>
-      <p>Loading events...</p>
-    </div>
-  );
-
-  if (isError) return (
-    <div className="error-container">
-      <p>Error loading events: {error}</p>
-    </div>
-  );
-
-  if (!events || events.length === 0) return (
-    <div className="error-container">
-      <p>No events found.</p>
-    </div>
-  );
-
-  return (
-    <div className="container mt-5">
-      <h1 className="text-center">Event Pages</h1>
-      <div className="row">
-        {events.map((event) => (
-          <div className="col-md-4 " key={event.id}>
-            <div className="card mt-4 event-card">
-              <div className="card-img-overlay d-flex justify-content-end align-items-start">
-                <span className="badge badge-primary">{event.status}</span>
-              </div>
-              <img
-                src={defaultImage}
-                className="card-img-top"
-                alt={event.name}
-                style={{borderRadius: "8px"}}
-              />
-              <div className="card-body custom-card-body">
-                <h5 className="card-title " style={{}}>{event.title}</h5>
-                <p className="card-text">Date: {event.startTime}</p>
-                <p className="card-text">Location: {event.location}</p>
-                <p className="card-text">{event.description}</p>
-                <button className="btn btn-primary">Reserve Spot</button>
-              </div>
-            </div>
-          </div>
-
-        ))}
+  if (isLoading)
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+        <p>Loading events...</p>
       </div>
-    </div>
-  );
+    );
+
+  if (isError)
+    return (
+      <div className="error-container">
+        <p>Error loading events: {error}</p>
+      </div>
+    );
+
+  if (!events || events.length === 0)
+    return (
+      <div className="error-container">
+        <p>No events found.</p>
+      </div>
+    );
+
+    return (
+      <div className="container mt-5">
+        <h1 className="text-center">Event Page</h1>
+        <h2 className="text-start">Events</h2>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+          {events.map((event) => (
+            <div className="col" key={event.id}>
+              <EventCard {...event} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+    
 };
 
 export default EventPage;
