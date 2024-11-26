0bc093a35f11e625cdc4bb2dd1aadee294d25f0b
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-25 22:30:09 -0600

    Reworked the Header for the event page

--- client/src/components/Events/Header2.jsx
+++ client/src/components/Events/Header2.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Header2.css";
-import SearchBar from "../HelperComponents/SearchBar";
+// import SearchBar from "../HelperComponents/SearchBar";
+import search_icon from "../../assets/search.svg";
+// import Sort2 from "../Sidebar/Sort2";
+
 const Header2 = () => {
   return (
     <>
@@ -8,15 +11,24 @@ const Header2 = () => {
         <div>
           <h1 className="event-page-heading-title">Events</h1>
         </div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <SearchBar />
+        <div className="event-page-heading-filter-a">
+          {/* <SearchBar /> */}
+          <div className="searchBox">
+            <input type="text" placeholder="Search"></input>
+            <img src={search_icon} alt="" className="navbar-search-icon" />
+          </div>
 
-          <select name="idk">
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-          </select>
+          <div className="event-page-heading-sort">
+            <span className="event-page-heading-span"> Sort By</span>
+            {/* <Sort2 /> */}
+
+            <select className="form-select" aria-label="Sorting options">
+              <option value="newestFirst">Newest First</option>
+              <option value="oldestFirst">Oldest First</option>
+              <option value="startTimeAsc">Start Time Ascending</option>
+              <option value="startTimeDesc">Start Time Descending</option>
+            </select>
+          </div>
         </div>
       </div>
     </>
