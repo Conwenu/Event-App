b9725bf5078be9a4002a09f72c603c349060cfae
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-27 22:09:20 -0600

    added state to the filter components within this header.

--- client/src/components/Events/Header2.jsx
+++ client/src/components/Events/Header2.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header2.css";
-// import SearchBar from "../HelperComponents/SearchBar";
 import search_icon from "../../assets/search.svg";
-// import Sort2 from "../Sidebar/Sort2";
+import SortingFilters from "../Sidebar/SortingFilters";
 
-const Header2 = () => {
+const Header2 = ({
+  selectedSorting,
+  onSortingChange,
+  searchFilters,
+  onSearchChange,
+}) => {
   return (
     <>
       <div className="event-page-heading">
@@ -12,22 +16,28 @@ const Header2 = () => {
           <h1 className="event-page-heading-title">Events</h1>
         </div>
         <div className="event-page-heading-filter-a">
-          {/* <SearchBar /> */}
           <div className="searchBox">
-            <input type="text" placeholder="Search"></input>
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchFilters}
+              onChange={(e) => onSearchChange(e.target.value)}
+            ></input>
             <img src={search_icon} alt="" className="navbar-search-icon" />
           </div>
 
           <div className="event-page-heading-sort">
             <span className="event-page-heading-span"> Sort By</span>
-            {/* <Sort2 /> */}
-
-            <select className="form-select" aria-label="Sorting options">
+            <SortingFilters
+              selectedSorting={selectedSorting}
+              onSortingChange={onSortingChange}
+            />
+            {/* <select className="form-select" aria-label="Sorting options">
               <option value="newestFirst">Newest First</option>
               <option value="oldestFirst">Oldest First</option>
               <option value="startTimeAsc">Start Time Ascending</option>
               <option value="startTimeDesc">Start Time Descending</option>
-            </select>
+            </select> */}
           </div>
         </div>
       </div>
