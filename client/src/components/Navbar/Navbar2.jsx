73cc53e4cf156113b51926662019af027ad633c9
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-27 22:09:39 -0600

    made the logo go to home when clicked on

--- client/src/components/Navbar/Navbar2.jsx
+++ client/src/components/Navbar/Navbar2.jsx
@@ -7,7 +7,9 @@ const Navbar2 = () => {
   return (
     <>
       <div className="customNavbar">
-        <img src={logo} alt="" className="navbar-logo"></img>
+        <a href="/">
+          <img src={logo} alt="" className="navbar-logo"></img>
+        </a>
 
         <div className="searchBox">
           <input type="text" placeholder="Search"></input>
@@ -17,12 +19,12 @@ const Navbar2 = () => {
         <div className="navbar-r">
           <ul className="navbar-r navbar-r-ul">
             <li className="sign-up-button-container">
-              <a href="/#" className="menu-title navbar-sign-up">
+              <a href="/signup" className="menu-title navbar-sign-up">
                 Sign Up
               </a>
             </li>
             <li className="log-in-button-container">
-              <a href="/#" className="menu-title navbar-log-in nav-btn">
+              <a href="/login" className="menu-title navbar-log-in nav-btn">
                 Log In
               </a>
             </li>
