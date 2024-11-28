e4b180b3ea130b85e8886352fdfd9cf34ab0107d
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-27 22:10:13 -0600

    finished implementing all the filters and added state to them

--- client/src/components/Sidebar/Sidebar2.jsx
+++ client/src/components/Sidebar/Sidebar2.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Sidebar2.css";
 import TimeBasedFilters from "./TimeBasedFilters.jsx";
+import EventCharacteristicsFilters from "./EventCharacteristicsFilters.jsx";
 const Sidebar2 = ({
   timeFilter,
   onTimeFilterChange,
@@ -9,6 +10,15 @@ const Sidebar2 = ({
   onDateRangeChange,
   selectedYear,
   setSelectedYear,
+  selectedMonth,
+  setSelectedMonth,
+  selectedEventStatus,
+  onEventStatusChange,
+  selectedReservationAbility,
+  onReservationAbilityChange,
+  durationFilter,
+  handleDurationChange,
+  resetFilters,
 }) => {
   const [filterOpen, setFilterOpen] = useState(false);
   const [statusOpen, setStatusOpen] = useState(false);
@@ -38,6 +48,8 @@ const Sidebar2 = ({
               onTimeFilterChange={onTimeFilterChange}
               onDateRangeChange={onDateRangeChange}
               setSelectedYear={setSelectedYear}
+              selectedMonth={selectedMonth}
+              setSelectedMonth={setSelectedMonth}
             />
           ) : (
             <></>
@@ -54,6 +66,16 @@ const Sidebar2 = ({
               }`}
             ></i>
           </div>
+          {statusOpen ? (
+            <EventCharacteristicsFilters
+              selectedEventStatus={selectedEventStatus}
+              onEventStatusChange={onEventStatusChange}
+              selectedReservationAbility={selectedReservationAbility}
+              onReservationAbilityChange={onReservationAbilityChange}
+            />
+          ) : (
+            <></>
+          )}
           <hr />
           <div
             className="sidebar-section"
@@ -66,6 +88,26 @@ const Sidebar2 = ({
               }`}
             ></i>
           </div>
+          {availabilityOpen && (
+            <div>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={selectedReservationAbility.includes("available")}
+                  onChange={() => onReservationAbilityChange("available")}
+                />
+                {" Events with Available Spots"}
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={selectedReservationAbility.includes("fullyBooked")}
+                  onChange={() => onReservationAbilityChange("fullyBooked")}
+                />
+                {" Fully Booked Events"}
+              </label>
+            </div>
+          )}
           <hr />
           <div
             className="sidebar-section"
@@ -78,9 +120,35 @@ const Sidebar2 = ({
               }`}
             ></i>
           </div>
+          {durationOpen && (
+            <div>
+              <label>
+                {/* Min Duration (in minutes): */}
+                <input
+                  type="number"
+                  value={durationFilter.min}
+                  min="0"
+                  onChange={(e) => handleDurationChange("min", e.target.value)}
+                  placeholder="Min duration (minutes)"
+                />
+              </label>
+              <label>
+                {/* Max Duration (in minutes): */}
+                <input
+                  type="number"
+                  value={durationFilter.max}
+                  min="0"
+                  onChange={(e) => handleDurationChange("max", e.target.value)}
+                  placeholder="Max duration (minutes)"
+                />
+              </label>
+            </div>
+          )}
           <hr />
         </div>
-        <div className="reset-filters">Reset Filters</div>
+        <div className="reset-filters" onClick={resetFilters}>
+          Reset Filters
+        </div>
       </div>
     </>
   );
