900bd6cbcb7d1bc590da816aa41ccee07e0fa382
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-11-29 22:06:08 -0600

    made the event page header more responsive for smaller screens

--- client/src/components/Events/Header2.jsx
+++ client/src/components/Events/Header2.jsx
@@ -2,42 +2,74 @@ import React from "react";
 import "./Header2.css";
 import search_icon from "../../assets/search.svg";
 import SortingFilters from "../Sidebar/SortingFilters";
+import FilterIcon from "../../assets/Filter_3_Lines.png";
 
 const Header2 = ({
   selectedSorting,
   onSortingChange,
   searchFilters,
   onSearchChange,
+  toggleModal,
 }) => {
   return (
     <>
       <div className="event-page-heading">
-        <div>
-          <h1 className="event-page-heading-title">Events</h1>
-        </div>
+        {/* Title Section */}
+        <h1 className="event-page-heading-title">Events</h1>
+
+        {/* Desktop Header */}
         <div className="event-page-heading-filter-a">
+          {/* Search Bar */}
           <div className="searchBox">
             <input
               type="text"
               placeholder="Search"
               value={searchFilters}
               onChange={(e) => onSearchChange(e.target.value)}
-            ></input>
-            <img src={search_icon} alt="" className="navbar-search-icon" />
+            />
+            <img
+              src={search_icon}
+              alt="Search Icon"
+              className="navbar-search-icon"
+            />
           </div>
 
+          {/* Sorting Filters */}
           <div className="event-page-heading-sort">
-            <span className="event-page-heading-span"> Sort By</span>
+            <span className="event-page-heading-span">Sort By</span>
             <SortingFilters
               selectedSorting={selectedSorting}
               onSortingChange={onSortingChange}
             />
-            {/* <select className="form-select" aria-label="Sorting options">
-              <option value="newestFirst">Newest First</option>
-              <option value="oldestFirst">Oldest First</option>
-              <option value="startTimeAsc">Start Time Ascending</option>
-              <option value="startTimeDesc">Start Time Descending</option>
-            </select> */}
+          </div>
+        </div>
+
+        {/* Mobile Header */}
+        <div className="event-page-heading-mobile">
+          {/* Filter Icon */}
+          <img
+            src={FilterIcon}
+            alt="Filter"
+            className="mobile-filter-icon"
+            onClick={() => {
+              toggleModal();
+              // Add functionality for the filter icon here
+            }}
+          />
+
+          {/* Search Bar */}
+          <div className="searchBox">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchFilters}
+              onChange={(e) => onSearchChange(e.target.value)}
+            />
+            <img
+              src={search_icon}
+              alt="Search Icon"
+              className="navbar-search-icon"
+            />
           </div>
         </div>
       </div>
