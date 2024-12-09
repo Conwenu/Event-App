9fcf29348002a7a9b29beb0ada10479330a6e278
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-12-08 21:46:33 -0600

    Added a userprofile path.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EventsPage from './components/Events/EventsPage';
 import EventPage from './components/Events/EventPage';
 import SignInPage from './components/SignIn/SignInPage';
 import RegisterPage from './components/Register/RegisterPage';
 import EventCard from './components/Events/EventCard';
 // import Navbar from './components/Navbar/Navbar';
 import Navbar2 from './components/Navbar/Navbar2';
+import UserProfile from './components/UserProfile/UserProfile';
 function App() {
   return (
     <Router>
@@ -17,10 +19,12 @@ function App() {
       </div>
       <div className="App">
         <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/" element={<EventPage />} />
-          <Route path="/api/events" element={<EventPage />} />
+          <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/login" element={<SignInPage />} />
+          <Route path="/profile/:userId" element={<UserProfile />} />
+          <Route path="/" element={<EventsPage />} />
+          <Route path="/api/events" element={<EventsPage />} />
+          <Route path="/api/events/:eventId" element={<EventPage />} />
           <Route path="/test/event" element={<EventCard />} />
         </Routes>
       </div>
