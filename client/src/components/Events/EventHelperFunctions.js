e44ed7703cb4fff1b305962557938db1f4267fae
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-12-25 15:56:17 -0600

    Added a function that determines the status of an event and returns it as well as updated the color mapped to each event status

--- client/src/components/Events/EventHelperFunctions.js
+++ client/src/components/Events/EventHelperFunctions.js
@@ -2,7 +2,7 @@ const EventHelperFunctions = {
     mapStatusToColor(status) {
         switch (status) {
           case "SCHEDULED":
-            return "bg-secondary";
+            return "bg-primary";
           case "IN_PROGRESS":
             return "bg-warning";
           case "COMPLETED":
@@ -10,8 +10,28 @@ const EventHelperFunctions = {
           case "CANCELLED":
             return "bg-danger";
           default:
-            return "bg-secondary"; 
+            return "bg-primary"; 
         }
+    },
+
+    determineStatus(startTime, endTime, status)  {
+      if (status === "CANCELLED") {
+        return "CANCELLED";
+      }
+    
+      const now = new Date();
+      const start = new Date(startTime);
+      const end = new Date(endTime);
+    
+      if (now < start) {
+        return "SCHEDULED";
+      } else if (now >= start && now <= end) {
+        return "IN_PROGRESS";
+      } else if (now > end) {
+        return "COMPLETED";
+      }
+    
+      return "SCHEDULED"; 
     },
 };
 
