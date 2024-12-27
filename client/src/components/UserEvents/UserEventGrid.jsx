e9d7c6b16280a81d11f3147cd3d70ebf59d3d8a4
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-12-26 20:14:43 -0600

    New component and styling that will display the events a user has created.

---
+++ client/src/components/UserEvents/UserEventGrid.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./UserEventGrid.css";
+const UserEventGrid = () => {
+  return (
+    <div>
+      <h2>Events Created</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+        reiciendis molestiae facere! Ea quibusdam nulla ipsa temporibus
+        assumenda voluptatibus aut facere adipisci quis optio? Corporis nemo
+        quidem voluptates neque quibusdam!
+      </p>
+    </div>
+  );
+};
+
+export default UserEventGrid;
