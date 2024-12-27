65f0d505da4ecfddb627674db76b544891474f97
Chigoziri <ChigoziriOnwenu@gmail.com>
2024-12-26 20:16:35 -0600

    Implemented the filter functionality. Still needs some work but it's nearly complete.

--- client/src/components/Events/EventsPage.jsx
+++ client/src/components/Events/EventsPage.jsx
@@ -1,33 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./EventsPage.css";
-// import { Badge } from "react-bootstrap";
-// import EventHelperFunctions from "./EventHelperFunctions.js";
 import EventCard from "./EventCard.jsx";
 import Sidebar from "../Sidebar/Sidebar.jsx";
-// import SortingFilters from "../Sidebar/SortingFilters.jsx";
 import Header2 from "./Header2.jsx";
 import Sidebar2 from "../Sidebar/Sidebar2.jsx";
-import SortingFilters from "../Sidebar/SortingFilters.jsx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 const API_URL = process.env.REACT_APP_API_URL;
-
-// function formatDate(isoTime) {
-//   const date = new Date(isoTime);
-
-//   const month = String(date.getMonth() + 1).padStart(2, "0");
-//   const day = String(date.getDate()).padStart(2, "0");
-//   const year = date.getFullYear();
-
-//   return `${month}/${day}/${year}`;
-// }
-
-// function getMonth(isoTime) {
-//   const date = new Date(isoTime);
-//   var monthName = new Intl.DateTimeFormat("en-US", { month: "long" }).format;
-//   var longName = monthName(date);
-//   return `${longName}`
-// }
 
 const EventsPage = () => {
   const [events, setEvents] = useState([]);
@@ -35,16 +14,52 @@ const EventsPage = () => {
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  // const [sortOpen, setSortOpen] = useState(false);
-  // const [selectedSorting, setSelectedSorting] = useState("");
-  // const handleSortingFilterChange = (value) => {
-  //   setSelectedSorting(value);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // const updateSearchParams = (filters) => {
+  //   const currentParams = Object.fromEntries(searchParams.entries());
+  //   const updatedParams = { ...currentParams, ...filters };
+
+  //   Object.keys(updatedParams).forEach((key) => {
+  //     if (
+  //       updatedParams[key] === undefined ||
+  //       updatedParams[key] === null ||
+  //       updatedParams[key] === ""
+  //     ) {
+  //       delete updatedParams[key];
+  //     }
+  //   });
+
+  //   setSearchParams(updatedParams);
   // };
+
+  const updateSearchParams = (filters) => {
+    const currentParams = Object.fromEntries(searchParams.entries());
+    const updatedParams = { ...currentParams, ...filters };
+
+    Object.keys(updatedParams).forEach((key) => {
+      if (
+        updatedParams[key] === undefined ||
+        updatedParams[key] === null ||
+        updatedParams[key] === "" ||
+        (Array.isArray(updatedParams[key]) && updatedParams[key].length === 0)
+      ) {
+        delete updatedParams[key];
+      }
+    });
+
+    setSearchParams(updatedParams);
+  };
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get(`${API_URL}/events`);
+        const apiParams = mapQueryToApiParams(
+          Object.fromEntries(searchParams.entries())
+        );
+        const response = await axios.get(`${API_URL}/events2`, {
+          params: apiParams,
+        });
         setEvents(response.data.events);
       } catch (err) {
         setIsError(true);
@@ -55,7 +70,49 @@ const EventsPage = () => {
     };
 
     fetchEvents();
-  }, []);
+  }, [searchParams]);
+
+  const mapQueryToApiParams = (params) => {
+    const apiParams = {};
+
+    if (params.timeFilter) {
+      apiParams.timeFilter = params.timeFilter;
+    }
+    if (params.selectedYear) {
+      apiParams.selectedYear = params.selectedYear;
+    }
+    if (params.selectedMonth) {
+      apiParams.selectedMonth = params.selectedMonth;
+    }
+    if (params.selectedWeekday) {
+      apiParams.selectedWeekday = params.selectedWeekday;
+    }
+    if (params.startDate) {
+      apiParams.startDate = params.startDate;
+    }
+    if (params.endDate) {
+      apiParams.endDate = params.endDate;
+    }
+    if (params.sort) {
+      apiParams.sort = params.sort;
+    }
+    if (params.minDuration) {
+      apiParams.minDuration = params.minDuration;
+    }
+    if (params.maxDuration) {
+      apiParams.maxDuration = params.maxDuration;
+    }
+    if (params.eventStatus) {
+      apiParams.eventStatus = params.eventStatus;
+    }
+    if (params.reservationAbility) {
+      apiParams.reservationAbility = params.reservationAbility;
+    }
+    if (params.searchQuery) {
+      apiParams.searchQuery = params.searchQuery;
+    }
+    return apiParams;
+  };
 
   const [timeFilter, setTimeFilter] = useState("");
 
@@ -65,6 +122,20 @@ const EventsPage = () => {
   const [selectedSorting, setSelectedSorting] = useState("");
   const [selectedMonth, setSelectedMonth] = useState(() => {
     return String(new Date().getMonth() + 1).padStart(2, "0");
+  });
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const [selectedWeekday, setSelectedWeekday] = useState(() => {
+    const currentDay = new Date().getDay();
+    return weekdays[currentDay];
   });
 
   const [multipleFilters, setMultipleFilters] = useState({
@@ -79,11 +150,32 @@ const EventsPage = () => {
 
   const [searchFilters, setSearchFilters] = useState("");
 
-  const getCurrentDateFormatted = () => {
-    // Get the current date
-    var currentDate = new Date();
+  // const handleSearchChange = (e) => {
+  //   const value = e.target.value;
+  //   setSearchFilters(value);
+  //   if (e.key === "Enter") {
+  //     updateSearchParams({ searchQuery: value });
+  //   }
+  // };
 
-    // Get the year, month, and day
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchFilters(value);
+    if (e.key === "Enter") {
+      if (value.trim() === "") {
+        updateSearchParams({ searchQuery: null }); // Remove search query if empty
+      } else {
+        updateSearchParams({ searchQuery: value });
+      }
+    }
+  };
+
+  const handleSearchButtonClick = () => {
+    updateSearchParams({ searchQuery: searchFilters });
+  };
+
+  const getCurrentDateFormatted = () => {
+    var currentDate = new Date();
     var year = currentDate.getFullYear();
     var month = currentDate.getMonth() + 1; // Months are 0-indexed, so we add 1
     var day = currentDate.getDate();
@@ -118,27 +210,160 @@ const EventsPage = () => {
     };
   }, []);
 
-  const handleTimeFilterChange = (value, year) => {
+  // const handleTimeFilterChange = (value, data) => {
+  //   const year = data?.year;
+  //   const month = data?.month;
+  //   const weekday = data?.weekday;
+  //   setTimeFilter(value);
+  //   updateSearchParams({ timeFilter: value });
+  //   if (value == "year" && year) {
+  //     setSelectedYear(year);
+  //     updateSearchParams({ timeFilter: value, selectedYear: year });
+  //   } else if (value == "month" && month) {
+  //     setSelectedMonth(data.month);
+  //     updateSearchParams({
+  //       timeFilter: value,
+  //       selectedYear: data.year,
+  //       selectedMonth: data.month,
+  //     });
+  //   } else if (value == "weekday" && weekday) {
+  //     setSelectedMonth(data.weekday);
+  //     updateSearchParams({ timeFilter: value, selectedWeekday: weekday });
+  //   }
+  // };
+
+  // const handleTimeFilterChange = (value, data) => {
+  //   const year = data?.year;
+  //   const month = data?.month;
+  //   const weekday = data?.weekday;
+
+  //   setTimeFilter(value); // Set the new time filter
+
+  //   // Base parameters that will always be included
+  //   let updatedParams = { timeFilter: value };
+
+  //   if (value === "year" && year) {
+  //     // If the filter is year, update the selected year
+  //     setSelectedYear(year);
+  //     updatedParams = { ...updatedParams, selectedYear: year };
+  //   } else if (value === "month" && month) {
+  //     // If the filter is month, update the selected year and month
+  //     setSelectedMonth(month);
+  //     updatedParams = {
+  //       ...updatedParams,
+  //       selectedYear: data.year,
+  //       selectedMonth: month,
+  //     };
+  //   } else if (value === "weekday" && weekday) {
+  //     // If the filter is weekday, update the selected weekday
+  //     setSelectedMonth(data.weekday);
+  //     updatedParams = { ...updatedParams, selectedWeekday: weekday };
+  //   }
+
+  //   // Remove irrelevant parameters based on the selected time filter
+  //   if (value !== "year" && value !== "month") {
+  //     delete updatedParams.selectedYear; // Remove selectedYear if not filtering by year
+  //   }
+  //   if (value !== "month") {
+  //     delete updatedParams.selectedMonth; // Remove selectedMonth if not filtering by month
+  //   }
+  //   if (value !== "weekday") {
+  //     delete updatedParams.selectedWeekday; // Remove selectedWeekday if not filtering by weekday
+  //   }
+
+  //   // Remove other parameters that aren't needed
+  //   const currentParams = Object.fromEntries(searchParams.entries());
+  //   Object.keys(currentParams).forEach((key) => {
+  //     // Ensure that irrelevant params are removed from the current URL
+  //     if (
+  //       (value === "year" && key !== "selectedYear" && key !== "timeFilter") ||
+  //       (value === "month" &&
+  //         key !== "selectedMonth" &&
+  //         key !== "selectedYear" &&
+  //         key !== "timeFilter") ||
+  //       (value === "weekday" &&
+  //         key !== "selectedWeekday" &&
+  //         key !== "timeFilter")
+  //     ) {
+  //       delete updatedParams[key];
+  //     }
+  //   });
+
+  //   // Update the search parameters
+  //   setSearchParams(updatedParams); // Ensure this properly updates the URL
+  // };
+
+  const handleTimeFilterChange = (value, data) => {
+    const year = data?.year;
+    const month = data?.month;
+    const weekday = data?.weekday;
+    const currentParams = Object.fromEntries(searchParams.entries());
     setTimeFilter(value);
-    setStartDate("");
-    // if(value == "today")
-    //   {
-    //     setStartDate(getCurrentDateFormatted());
-    //   }
-    if (year) {
+
+    let updatedParams = { timeFilter: value };
+
+    // Keep the other filters intact
+    if (currentParams.eventStatus)
+      updatedParams.eventStatus = currentParams.eventStatus;
+    if (currentParams.reservationAbility)
+      updatedParams.reservationAbility = currentParams.reservationAbility;
+    if (currentParams.minDuration)
+      updatedParams.minDuration = currentParams.minDuration;
+    if (currentParams.maxDuration)
+      updatedParams.maxDuration = currentParams.maxDuration;
+    if (currentParams.searchQuery)
+      updatedParams.searchQuery = currentParams.searchQuery;
+
+    // Time filter logic
+    if (value === "year" && year) {
       setSelectedYear(year);
+      updatedParams = { ...updatedParams, selectedYear: year };
+      setStartDate("");
+    } else if (value === "month" && month) {
+      setSelectedMonth(month);
+      updatedParams = {
+        ...updatedParams,
+        selectedYear: data.year,
+        selectedMonth: month,
+      };
+      setStartDate("");
+    } else if (value === "weekday" && weekday) {
+      setSelectedWeekday(weekday);
+      updatedParams = { ...updatedParams, selectedWeekday: weekday };
+      setStartDate("");
     }
+
+    if (currentParams.startDate) {
+      updatedParams.startDate = currentParams.startDate;
+    }
+
+    // Clean up non-time filters from the updatedParams if they're not needed
+    if (value !== "year" && value !== "month") {
+      delete updatedParams.selectedYear;
+    }
+    if (value !== "month") {
+      delete updatedParams.selectedMonth;
+    }
+    if (value !== "weekday") {
+      delete updatedParams.selectedWeekday;
+    }
+
+    // Update the searchParams with the modified params
+    setSearchParams(updatedParams);
   };
 
   const handleSortingFilterChange = (value) => {
     setSelectedSorting(value);
+    updateSearchParams({ sort: value });
   };
 
   const handleDateRangeChange = (name, value) => {
     if (name === "startDate") {
       setStartDate(value);
+      updateSearchParams({ [name]: value });
     } else if (name === "endDate") {
       setEndDate(value);
+      updateSearchParams({ [name]: value });
     }
   };
 
@@ -150,6 +375,12 @@ const EventsPage = () => {
       } else {
         newFilterValues.push(value);
       }
+
+      updateSearchParams({
+        ...prevState,
+        [filterType]: newFilterValues.join(","),
+      });
+
       return { ...prevState, [filterType]: newFilterValues };
     });
   };
@@ -162,6 +393,7 @@ const EventsPage = () => {
       ...prevState,
       [name]: value,
     }));
+    updateSearchParams({ [name + "Duration"]: value });
   };
 
   const resetFilters = () => {
@@ -174,13 +406,24 @@ const EventsPage = () => {
       eventStatus: [],
       reservationAbility: [],
     });
-
     setSelectedYear(new Date().getFullYear());
-    setDurationFilter({
-      min: "",
-      max: "",
-    });
+    setDurationFilter({ min: "", max: "" });
     setSearchFilters("");
+
+    updateSearchParams({
+      timeFilter: null,
+      startDate: null,
+      endDate: null,
+      sorting: null,
+      selectedYear: null,
+      selectedMonth: null,
+      selectedWeekday: null,
+      eventStatus: null,
+      reservationAbility: null,
+      minDuration: null,
+      maxDuration: null,
+      searchQuery: null,
+    });
   };
 
   // const defaultImage =
@@ -207,15 +450,6 @@ const EventsPage = () => {
       </>
     );
 
-  if (!events || events.length === 0)
-    return (
-      <>
-        <div className="error-container">
-          <p>No events found.</p>
-        </div>
-      </>
-    );
-
   return (
     <>
       <div className="full-events-page">
@@ -224,6 +458,8 @@ const EventsPage = () => {
           onSortingChange={handleSortingFilterChange}
           searchFilters={searchFilters}
           onSearchChange={setSearchFilters}
+          handleSearchChange={handleSearchChange}
+          handleSearchButtonClick={handleSearchButtonClick}
           toggleModal={toggleModal}
           isMobile={isMobile}
         />
@@ -239,6 +475,8 @@ const EventsPage = () => {
               setSelectedYear={setSelectedYear}
               selectedMonth={selectedMonth}
               setSelectedMonth={setSelectedMonth}
+              selectedWeekday={selectedWeekday}
+              setSelectedWeekday={setSelectedWeekday}
               selectedEventStatus={multipleFilters.eventStatus}
               onEventStatusChange={(value) =>
                 handleMultipleFilterChange("eventStatus", value)
@@ -257,11 +495,20 @@ const EventsPage = () => {
               toggleModal={toggleModal}
             />
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 event-container">
-              {events.map((event) => (
-                <div className="col" key={event.id}>
-                  <EventCard {...event} />
+              {events && events.length > 0 ? (
+                events.map((event) => (
+                  <div className="col" key={event.id}>
+                    <EventCard {...event} />
+                  </div>
+                ))
+              ) : (
+                <div
+                  className="d-flex justify-content-center align-items-center w-100"
+                  style={{ height: "200px" }}
+                >
+                  <p className="text-center mb-0">No events found.</p>
                 </div>
-              ))}
+              )}
             </div>
 
             {/* <div>
@@ -303,6 +550,8 @@ const EventsPage = () => {
               setSelectedYear={setSelectedYear}
               selectedMonth={selectedMonth}
               setSelectedMonth={setSelectedMonth}
+              selectedWeekday={selectedWeekday}
+              setSelectedWeekday={setSelectedWeekday}
               selectedEventStatus={multipleFilters.eventStatus}
               onEventStatusChange={(value) =>
                 handleMultipleFilterChange("eventStatus", value)
@@ -352,11 +601,20 @@ const EventsPage = () => {
           <h2 className="text-start">Events</h2> */}
 
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 event-container">
-              {events.map((event) => (
-                <div className="col" key={event.id}>
-                  <EventCard {...event} />
+              {events && events.length > 0 ? (
+                events.map((event) => (
+                  <div className="col" key={event.id}>
+                    <EventCard {...event} />
+                  </div>
+                ))
+              ) : (
+                <div
+                  className="d-flex justify-content-center align-items-center w-100"
+                  style={{ height: "200px" }}
+                >
+                  <p className="text-center mb-0">No events found.</p>
                 </div>
-              ))}
+              )}
             </div>
           </div>
         )}
