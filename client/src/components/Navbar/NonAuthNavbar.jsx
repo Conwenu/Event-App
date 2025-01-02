235099509a4db0bdc6e958e80d40f7b9005bd124
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-01 20:48:04 -0600

    Created and styled a navbar for non authenthicated users

---
+++ client/src/components/Navbar/NonAuthNavbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../../assets/EventAppLogo2.png";
+import "./NonAuthNavbar.css";
+const NonAuthNavbar = ({ toggleTheme, isDarkMode }) => {
+  return (
+    <>
+      <div>
+        <nav className="non-auth-navbar navbar navbar-light justify-content-between">
+          <a className="non-auth-navbar-logo navbar-brand" href="/">
+            <img className="logo" src={logo} alt="logo" width="80px"></img>
+          </a>
+          <div className="non-auth-navbar-right-side">
+            <i
+              className={`bi bi-${
+                isDarkMode ? "moon" : "sun"
+              } fs-3 non-auth-navbar-theme-toggle`}
+              onClick={toggleTheme}
+            ></i>
+            <a href="/signup">
+              <button
+                class="btn my-2 my-sm-0 non-auth-navbar-button"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </a>
+            <a href="/login">
+              <button
+                class="btn my-2 my-sm-0 non-auth-navbar-button"
+                type="button"
+              >
+                Log In
+              </button>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NonAuthNavbar;
