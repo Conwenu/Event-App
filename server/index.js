1e6554c5e44fd20193231e3666bc89817579b627
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-11 21:00:39 -0600

    added api limit, will change since I will host with aws soon

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const userRoutes = require('./users/userRoutes');
 const reservationRoutes = require('./reservations/reservationRoutes');
 const cors = require('cors');
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "1.3mb" }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
