5960e0fe1c9e2d663b65c4311be182cb428b6547
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-12 21:07:04 -0600

    fixed the login and registration functions in the service file.

--- server/auth/authService.js
+++ server/auth/authService.js
@@ -5,8 +5,24 @@ const { PrismaClient } = require('@prisma/client');
 const {AppError} = require('../helpers/AppError')
 const prisma = new PrismaClient();
 
-const login = async(userData) => {
+const login = async(userData, res) => {
     const {username, email, password} = userData;
+    
+    if (!username || !email) {
+        throw new AppError('Username or Email is required', 400);
+    }
+    if (!password) {
+        throw new AppError('Password is required', 400);
+    }
+    if(password.length < 8)
+    {
+        throw new AppError('Password must be at least 8 characters', 400);
+    }
+    if(password.length > 20)
+    {
+        throw new AppError('Password must be at most 20 characters' , 400);
+    }
+
     const existingUser = await prisma.user.findFirst({
         where: {
           OR: [
@@ -17,31 +33,99 @@ const login = async(userData) => {
     });
     
     if (!existingUser) {
-        throw new AppError('Invalid Username or Password');
+        throw new AppError('Invalid Credentials');
     }
 
     const isMatch = await bcrypt.compare(password, existingUser.password);
-
+     // Password is correct
     if (isMatch) {
-        // Password is correct, log the user in
+        // if(!existingUser.isAccountVerified)
+        // {
+        //     // Tell User to check for a confirmation email
+        //     throw new AppError('Please Check Your Email For Confirmation Email');
+        // }
     } else {
-        throw new AppError('Invalid Username or Password');
+        throw new AppError('Invalid Credentials');
         // Password is incorrect
     }
 
-    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: existingUser.id, username: existingUser.username, email: existingUser.email }, process.env.JWT_SECRET, {
         expiresIn: '1d' // Token expiration time
     });
-
     // Step 5: Respond with User Info and Token
+    res.cookie('token', token);
+    
     return {
         user: {
-            id: user.id,
-            username: user.username,
-            email: user.email
+            id: existingUser.id,
+            username: existingUser.username,
+            email: existingUser.email
         },
         token: token
     };
+}
+
+const register = async(userData) => {
+    const {username, email, password} = userData;
+    
+    if (!username || !email) {
+        throw new AppError('Username or Email is required', 400);
+    }
+    if(username.length < 8)
+    {
+        throw new AppError('Username must be at least 8 characters' , 400);
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        throw new AppError('Invalid email format' , 400);
+    }
+    if (!password) {
+        throw new AppError('Password is required' , 400);
+    }
+    if(password.length < 8)
+    {
+        throw new AppError('Password must be at least 8 characters' , 400);
+    }
+    if(password.length > 20)
+    {
+        throw new AppError('Password must be at most 20 characters' , 400);
+    }
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email: email },
+                    { username: username }
+                ]
+            }
+        });
+        
+        if (existingUser) {
+            throw new AppError('Email or Username is already registered' , 400);
+        }
+
+        // Create the new user
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword, 
+                isAccountVerified : false,
+            }
+        });
+
+        // Send email to the new user to verify
+
+    } catch (error) {
+        throw new AppError('Server error, please try again later' , 500)
+    }
+}
+
+const logout = async(userData) => {
+
 }
 
 const verifyToken = (token) => {
@@ -55,4 +139,4 @@ const verifyToken = (token) => {
 
 
 
-module.exports = { login, verifyToken }
+module.exports = { login, verifyToken, register, logout }; 
