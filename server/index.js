a5024188e8afb99c88d71dbc8936dd326bf3d750
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-13 09:24:45 -0600

    removed a space

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cookieParser =  require('cookie-parser');
 const cors = require('cors');
 const app = express();
 app.use(express.json({ limit: "1.3mb" }));
-app.use(cors({origin : process.env.CLIENT_URL, method: ['GET', 'POST', 'PUT', 'DELETE '], credentials : true}));
+app.use(cors({origin : process.env.CLIENT_URL, method: ['GET', 'POST', 'PUT', 'DELETE'], credentials : true}));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api', eventRoutes);
