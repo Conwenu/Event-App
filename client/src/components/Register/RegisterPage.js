a9aaec3aa16026e98ccfc7033907585d2a7c9e26
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-12 20:58:06 -0600

    Fixed the Register Page

--- client/src/components/Register/RegisterPage.js
+++ client/src/components/Register/RegisterPage.js
@@ -1,14 +1,51 @@
 import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css"; 
 import "./RegisterPage.css"; 
-
+import * as Yup from "yup"
+import { Formik, Field, Form, ErrorMessage } from "formik"
+import axios from "axios";
+const API_URL = process.env.REACT_APP_API_URL;
+const AUTH_URL = process.env.REACT_APP_AUTH_URL;
+const registrationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Please provide a valid email address")
+    .required("Please provide an email address"),
+  username: Yup.string()
+    .min(8, "Username must be at least 8 characters long")
+    .max(20, "Username must be at most 20 characters long")
+    .matches(
+      /^[a-zA-Z0-9]+$/,
+      "Username can only contain letters and numbers"
+    )
+    .required("Please provide a username"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(20, "Password must be at most 20 characters long")
+    .required("Please provide a Password"),
+});
 function RegisterPage() {
   const [showPassword, setShowPassword] = useState(false);
-
+  const navigate = useNavigate();
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
+
+  const handleRegistration = async (values) => {
+    try {
+      const response = await axios.post(`${AUTH_URL}/register`, values);
+      
+      if (response.status === 200) {
+        navigate("/login");
+      }
+      console.log(response);
+      console.log(values);
+    } catch (error) {
+      console.error("Error registering:", error.response.data.error);
+    }
+  };
+
 
   return (
     <div className="register-container">
@@ -17,40 +54,75 @@ function RegisterPage() {
         <div className="card-body">
           <h2 className="fw-bold mb-4 text-center">Register</h2>
           
-          <div className="mb-3">
-            <input
-              className="form-control"
-              id="username"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              id="email"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-3 position-relative">
-            <input
-              className="form-control"
-              id="password"
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-            />
-            <i
-              className={`register-password-toggle password-toggle ${
-                showPassword ? "bi-eye-slash" : "bi-eye"
-              }`}
-              onClick={toggleShowPassword}
-            ></i>
-          </div>
-          
-          <button className="register-button btn w-100 mb-3" type="submit">
-            Sign up
-          </button>
+          <Formik
+            initialValues={{ email: "", username: "", password: "" }}
+            validationSchema={registrationSchema}
+            onSubmit={handleRegistration}
+          >
+            {({ setFieldValue }) => (
+              <Form>
+                 <label className = "register-form-label" htmlFor="username">Username</label>
+                <div className="mb-3">
+                  <Field
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+
+                <div className="mb-3">
+                <label className = "register-form-label" htmlFor="email">Email</label>
+                  <Field
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+
+
+                <div className="mb-3 position-relative">
+                <label className = "register-form-label" htmlFor="password">Password</label>
+                  <Field
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                  />
+                  <i
+                    className={`register-password-toggle password-toggle ${
+                      showPassword ? "bi-eye-slash" : "bi-eye"
+                    }`}
+                    onClick={toggleShowPassword}
+                  ></i>
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+
+                {/* Submit Button */}
+                <button className="register-button btn w-100 mb-3" type="submit">
+                  Sign up
+                </button>
+              </Form>
+            )}
+          </Formik>
          
           <div className="text-center">
             <p className="mb-0">
