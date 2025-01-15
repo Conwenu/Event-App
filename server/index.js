4bcfa4404b2da22891e60e1d736b50fd972cf96a
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-14 22:44:49 -0600

    added credentials on the backend to be applied before cors

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const eventRoutes = require('./events/eventRoutes');
 const userRoutes = require('./users/userRoutes');
 const reservationRoutes = require('./reservations/reservationRoutes');
 const authRoutes = require('./auth/authRoutes');
-const { verifyJWT } = require('./auth/authController');
+const { verifyJWT, credentials } = require('./auth/authController');
 const cookieParser =  require('cookie-parser');
 const cors = require('cors');
 const app = express();
 app.use(express.json({ limit: "1.3mb" }));
+app.use(credentials);
 app.use(cors({origin : process.env.CLIENT_URL, method: ['GET', 'POST', 'PUT', 'DELETE'], credentials : true}));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cookieParser());
@@ -21,27 +22,6 @@ app.use('/api', reservationRoutes);
 
 
 console.log(process.env.CLIENT_URL)
-// const authenticate = (req, res, next) => {
-//     const token = req.headers.authorization?.split(' ')[1]; // Extract token from Authorization header
-
-//     if (!token) {
-//         return res.status(401).send('Access denied. No token provided.');
-//     }
-
-//     try {
-//         const decoded = verifyToken(token); // Verify the token
-//         req.user = decoded; // Attach the user info to the request object
-//         next(); // Proceed to the next middleware/route handler
-//     } catch (error) {
-//         return res.status(401).send(error.message);
-//     }
-// };
-
-// // Protect a route with the authenticate middleware
-// app.get('/protected', authenticate, (req, res) => {
-//     res.send(`Hello ${req.user.username}, you are authenticated!`);
-// });
-
 
 const PORT = process.env.PORT || 5000;
 
