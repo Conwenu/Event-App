d718946caaf9cf73e7a09c3ebd6bfb9552a76155
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-18 20:05:47 -0600

    made the axios export use the server url instead

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_AUTH_URL;
+const API_URL = process.env.REACT_APP_API_URL
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default axios.create({
     baseURL: BASE_URL
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: SERVER_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
