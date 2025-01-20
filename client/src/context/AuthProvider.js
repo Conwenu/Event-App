d3bb8f355fae40092b48466751f6dbef94dfb5a1
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-01-19 21:41:27 -0600

    Fixed the auth provider to implement session sotrage so it doesnt reset auth on refresh

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,15 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(() => {
+        const savedAuth = sessionStorage.getItem("auth");
+        return savedAuth ? JSON.parse(savedAuth) : {}; 
+    });
+
+    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || false);
+
+    useEffect(() => {
+        localStorage.setItem("persist", JSON.stringify(persist));
+    }, [persist]);
+
+    useEffect(() => {
+        if (auth && Object.keys(auth).length > 0) {
+            sessionStorage.setItem("auth", JSON.stringify(auth));
+        }
+    }, [auth]); 
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
+        <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export default AuthContext;
